85d46971aad6a131fbf25b2f3540087dc4549fef
sprahul333 <sprahul333@gmail.com>
2025-05-29 07:48:52 +0530

    Added SampleFiles

---
+++ src/SampleFiles.java
@@ -0,0 +1,10 @@
+public class SampleFiles {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Today is new day to learn Java.");
+
+        System.out.println("Understanding Pre and Post Operations in Java");
+    }
+}
