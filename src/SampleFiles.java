5d9c01d4d1ef3b670b1cadc8bbd1fd318feb0a6c
sprahul333 <sprahul333@gmail.com>
2025-05-29 07:50:03 +0530

    Added Some New changes to the files

--- src/SampleFiles.java
+++ src/SampleFiles.java
@@ -6,5 +6,15 @@ public class SampleFiles {
         System.out.println("Today is new day to learn Java.");
 
         System.out.println("Understanding Pre and Post Operations in Java");
+
+        System.out.println("This code demonstrates the use of pre-increment, post-increment, pre-decrement, and post-decrement operations in Java.");
+
+        System.out.println("Pre-increment: Increases the value by 1 before using it.");
+
+        System.out.println("Post-increment: Increases the value by 1 after using it.");
+
+        System.out.println("Pre-decrement: Decreases the value by 1 before using it.");
+
+        System.out.println("Post-decrement: Decreases the value by 1 after using it.");
     }
 }
