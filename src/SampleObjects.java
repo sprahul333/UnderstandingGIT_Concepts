c53d9e729f65a4f333833af748c01c12c18b4048
sprahul333 <sprahul333@gmail.com>
2025-05-30 07:33:50 +0530

    Adding new files related to objects

---
+++ src/SampleObjects.java
@@ -0,0 +1,11 @@
+public class SampleObjects {
+
+    public static void main(String[] args) {
+
+        Object o1=new Object();
+
+        Object o2=new Object();
+
+        System.out.println(o1==o2);
+    }
+}
